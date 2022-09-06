72ba1bed0fc683a4ef76020d216544f79e57398c
Jack <3474387017@qq.com>
2022-09-06 21:38:29 +0800

    First edition

---
+++ java/com/ss/video/rtc/demo/quickstart/Constants.java
@@ -0,0 +1,21 @@
+package com.ss.video.rtc.demo.quickstart;
+
+/**
+ * VolcEngineRTC 常量定义
+ */
+public class Constants {
+
+    //APPID 使用SDK前需要为自己的应用申请一个AppId，详情参见{https://www.volcengine.com/docs/6348/69865}
+    public static final String APPID = "630d5b3378d969018cd522a4";
+
+    //TOKEN 加入房间的时候需要使用token完成鉴权，详情参见{https://www.volcengine.com/docs/6348/70121}
+    public static final String TOKEN = "001630d5b3378d969018cd522a4NgAKOpAFuZMWYznOH2MAAAAABgAAADnOH2MBAD" +
+            "nOH2MCADnOH2MDADnOH2MEADnOH2MFADnOH2MgAIeY+Vs964MCmb47VUUFN8mF6xVDD3ZEbJoEkmT4MqiQ";
+
+    //INPUT_REGEX SDK 对房间名、用户名的限制是：非空且最大长度不超过128位的数字、大小写字母、@ . _ | -
+    public static final String INPUT_REGEX = "^[a-zA-Z0-9@._|-]{1,128}$";
+
+    public static final String ROOM_ID_EXTRA = "extra_room_id";
+
+    public static final String USER_ID_EXTRA = "extra_user_id";
+}
